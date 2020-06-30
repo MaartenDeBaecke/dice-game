45a968131e689d172ac73737a68c5fbb41ce0914
Maarten De Baecke <40885625+MaartenDeBaecke@users.noreply.github.com>
2020-06-30 12:33:07 +0200

    Banner issues solved

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function play(){
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  document.querySelectorAll("img")[3].setAttribute("src", "images/dice" + randomNumber1 + ".png");
-  document.querySelectorAll("img")[4].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  document.querySelectorAll("img")[4].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelectorAll("img")[5].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
   if (randomNumber1 > randomNumber2){
     document.querySelector("p").textContent = "Player 1 is the Winner!";
